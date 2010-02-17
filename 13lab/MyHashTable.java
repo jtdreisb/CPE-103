bc676a02c383d9f554af80d6e175de3275c0c3f6
Mathew Duhon <mduhon@jl05.csc.calpoly.edu>
2010-02-17 12:03:13 -0800

    Finished lab13

--- 13lab/MyHashTable.java
+++ 13lab/MyHashTable.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class MyHashTable<T>
 {
    LinkedList <T> [] table;
-   public HashTableSC (int count)
+   public MyHashTable (int count)
    {
       table = ((LinkedList <T> [] )new LinkedList[count]);
       for (int i =0; i<table.length; i++)
@@ -61,5 +61,60 @@ public class MyHashTable<T>
       {         
          ret+= table[i].size();
       }
+      return ret;
+   }
+   private class Iter implements Iterator<T> 
+   {
+      public int i, j;
+      
+      public Iter (LinkedList <T> [] table)
+      {
+         for (int i =0; i<table.length; i++)
+         {
+            if(table[i].size() != 0)
+            break;
+         }
+         j=0;
+      }
+      
+      public boolean hasNext()
+      {
+         if(table[i].size() > j+1)
+            return true;
+            
+         for ( int l = i+1 ; l<table.length; l++)
+         {
+            if(table[l].size() != 0)
+               return true;
+         }
+         return false;
+      }
+      
+      public T next()
+      {
+         if(table[i].size() > j+1)
+         {
+            j++;
+            return table[i].get(j);
+         }
+         j = 0;
+         for ( i = i+1 ; i<table.length; i++)
+         {
+            if(table[i].size() != 0)
+            {
+               return table[i].get(j);
+            }
+         }
+         throw new  NoSuchElementException();
+      }
+      public void remove()
+      {
+         throw new UnsupportedOperationException();
+      }     
+   }
+   
+   public Iterator<T> iterator()
+   {
+      return new Iter(table);
    }
 }
