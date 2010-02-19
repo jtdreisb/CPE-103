9dd30f453b101539bf5c6314728e8da5a6e53c0b
Jason Dreisbach <jtdreisb@jl05.csc.calpoly.edu>
2010-02-19 13:31:09 -0800

    Completed Program 4 functionality

--- project4/HashTable.java
+++ project4/HashTable.java
@@ -1,4 +1,5 @@
 /*** HashTable with open addressing* With quadratic probing* @version cpe 103 section 4** @author Mathew Duhon and Jason Dreisbach* @version Program 4*/
+import java.util.*;
 public class HashTable
 {
    private HashEntry[] table;
@@ -42,17 +43,137 @@ public class HashTable
       }
       public boolean hasNext()
       {
-         return cursor < table.length;
+         int i = cursor;
+         while(true)
+         {
+            if( i >= table.length)
+            {
+               return false;
+            }
+            if(table[i] != null)
+            {
+               if(table[i].isActive)
+                  break;
+            }
+            i++;
+         }
+         return true;
       }
       public Object next()
       {
-         
-         return null;
+         Object ret = null;
+         while(ret == null)
+         {
+            if( cursor >= table.length)
+            {
+               System.out.println(cursor +"=?"+table.length);
+               throw new NoSuchElementException();
+            }
+            if(table[cursor] != null)
+            {
+               if(table[cursor].isActive)
+                  ret = table[cursor].item;
+            }
+            cursor++;
+         }
+         return ret;
       }
       public void remove()
       {
          throw new UnsupportedOperationException();
       }
+   }
+
+   
+   
+   public Object find(Object item)
+   {
+      int index = findPosition(item);
+      return table[index] != null && table[index].isActive;
+   }
+   
+   
+   
+   public void insert(Object item)
+   {
+      int index = findPosition(item);
+      if(table[index] == null)
+      {
+         table[index] = new HashEntry(item);
+         count++;
+         if(count >= table.length/2)
+         {
+            rehash();
+         }
+      }
+      else 
+      {
+         if(!table[index].isActive)
+         {
+            table[index].isActive = true;            
+         }
+      }
+   }
+   
+   
+
+   
+   public void delete(Object item)
+   {
+      int index = findPosition(item);
+      if(table[index] != null)
+      {
+         table[index].isActive = false;
+         count--;
+      }
+   }
+   
+   
+   public int elementCount()
+   {
+      return count;
+   }
+   
+   
+   public boolean isEmpty()
+   {
+      return count == 0;
+   }
+   
+   
+   public void makeEmpty()
+   {
+      count = 0;
+      for( int i = 0; i < table.length; i++)
+      {
+         table[i] = null;
+      }
+   }
+   
+   
+   public void printTable()
+   { 
+      for( int i = 0; i <table.length; i++)
+      {
+         System.out.print("[" + i + "]: ");
+         if(table[i] != null)
+         {
+            System.out.print(table[i].item);
+            if(table[i].isActive)
+               System.out.println(", active");
+            else
+               System.out.println(", inactive");
+         }
+         else
+            System.out.println("empty");
+      }
+      System.out.println();
+   }
+   
+   
+   public Iterator iterator()
+   {
+      return new Iter(table);
    }
    
    
@@ -62,7 +183,6 @@ public class HashTable
      boolean found = false;
      while(!found)
      {
-        prime++;         
         found = true;
         for(int i = 2; i < prime; i++)
         {
@@ -72,7 +192,45 @@ public class HashTable
               break;
            }
         }
+        if(!found)
+            prime++;
      }
      return prime;
    }
+      
+   
+   private int findPosition(Object x)
+   {
+      int i = 0;
+      int hashValue = hash(x);
+      int index = hashValue;
+      while(table[index] != null &&  !x.equals(table[index].item))
+      {
+         i++;
+         index = (hashValue+i*i) % table.length;
+      }
+      return index;
+   }
+   
+   
+   private void rehash()
+   {
+      HashEntry[] temp = table;
+      table = new HashEntry[nextPrime(2*temp.length)];
+      count = 0;
+      for(int i = 0; i < temp.length; i++)
+      {
+         if(temp[i] != null && temp[i].isActive)
+         {
+            insert(temp[i].item);
+         }
+      }
+   }
+   
+   
+   private int hash(Object x)
+   {
+      return Math.abs(x.hashCode()) % table.length;
+   }
+
 }
--- project4/Student.java
+++ project4/Student.java
@@ -1,18 +1,72 @@
-public class Student
+/** * Student class to keep student ID and Name * provides a toString method
+ * * @version cpe 103 section 4 * * @author Mathew Duhon and Jason Dreisbach * @version Program 4 */
+ 
+public class Student 
 {
-   public static void main(String[] args)
+   
+   
+   /* 
+    * id holds the students id number
+    * each student's id is unique
+    */
+   private long id;
+   
+   
+   /*
+    * Name holds a string without spaces representing the student
+    */ 
+   private String name;
+   
+   
+   /**
+    * Constructs a student with a id and a name
+    *
+    * @Pre-conditions: none
+    * @Post-conditions: initialized student object
+    *
+    */
+   public Student (long num, String lName)
    {
-      /*int num = 16;
-      for(int i = 1; i < num; i++)
-         System.out.println((num % i));*/
-      new HashTable(1);
-      new HashTable(2);
-      new HashTable(6);
-      new HashTable(9);
-      new HashTable(11);
-      new HashTable(35);
-      new HashTable(17);
-      new HashTable(11000);
-      
+      id=num;
+      name=lName;
+   }
+   
+   
+   /**
+    * overiden equals method
+    *
+    * @Pre-conditions: other != null
+    * @Post-onditions: a 1 0 or -1 value representing the collation
+    *
+    */
+   public boolean equals(Object other)
+   {
+      return this.id == ((Student)other).id;
+   }
+   
+   
+   /**
+    * Returns a formatted student string
+    *
+    * @Pre-conditions: none
+    * @Post-conditions: Returns a string with id and name of student
+    *
+    */
+   public String toString()
+   {
+      return ("{ id: " + id + ", name: " + name+ " }");
+   }
+   
+   
+   /**
+    * Returns a formatted student string
+    *
+    * @Pre-conditions: none
+    * @Post-conditions: Returns a string with id and name of student
+    *
+    */
+   public int hashCode()
+   {
+      return new Long(id).hashCode();
    }
 }
